420df587ff4a6f4775462552d51e7a08914e2a55
grmelito <gr.melito@hotmail.com>
2020-06-07 21:28:48 -0300

    Controller de Anuncio

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const routes = express.Router();
 
 const CategoriaController = require('./controllers/CategoriaController')
+const AnuncioController = require('./controllers/AnuncioController')
+
 
 routes.get('/categorias', CategoriaController.index)
+
+routes.get('/anuncio', AnuncioController.index)
 
 module.exports = routes;
