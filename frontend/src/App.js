b4f5eeaa58c379eedc4f80de0ac23f51507595fe
EricaSobral <erica.sobral95@gmail.com>
2020-06-09 23:25:52 -0300

    testando configurações react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/header/Header'
+
 
 
 function App() {
   return (
+    <div>
+    <Header/>
     <h1>Teste FC</h1>
+
+    </div>
   );
 }
 
---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header className="headerHome">
+           
+                                  
+        </header>
+    );
+}
+
+export default Header;
