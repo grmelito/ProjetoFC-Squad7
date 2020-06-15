5d159a09fd15fdc79bcb528d90c4415e249bf68b
EricaSobral <erica.sobral95@gmail.com>
2020-06-15 02:56:18 -0300

    Iniciando header da página home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
-
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div>
-
-   </div>
+      <Header/>
+    </div>
   );
 }
 
--- frontend/src/components/Header/header.js
+++ frontend/src/components/Header/header.js
@@ -1,6 +1,28 @@
-import React from 'react'
-import '../../assets/css/reset.css'
-import '../../assets/css/header.css'
-import Button from 'react-bootstrap/Button';
+import React from 'react';
+import logo from '../../assets/img/logoAtualizada.png';
+import '../../assets/css/reset.css';
+import '../../assets/css/header.css';
 
 
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <div className="menu-header-home">
+                    <div className="logo-header">
+                        <a href="#"><img src={logo} alt="bfriend" /></a>
+                    </div>
+                    <nav className="btn-header-home">
+                        <button className="btn-cadastro-header">Cadastre-se</button>
+                        <button className="btn-entrar-header">Entrar</button>
+                    </nav>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+
+
+export default Header
