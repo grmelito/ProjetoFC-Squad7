d632dc69b6bbb80f63f966b54543f0a320b30fe2
EricaSobral <erica.sobral95@gmail.com>
2020-06-15 00:52:30 -0300

    Reconfigurando pastas e adicionando imagens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import Home from './pages/Home/Home';
-import Footer from './pages/Footer/Footer';
 
 
 
 function App() {
   return (
     <div>
-       <Home/>
-        
 
-      <Footer/>
-    </div>
+   </div>
   );
 }
 
---
+++ frontend/src/components/Header/header.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import '../../assets/css/reset.css'
+import '../../assets/css/header.css'
+import Button from 'react-bootstrap/Button';
+
+
