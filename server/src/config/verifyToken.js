90fecaceb019a46a8028d10d38d7aaf80c57e275
grmelito <gr.melito@hotmail.com>
2020-06-15 21:26:32 -0300

    Verificação do Token

--- server/src/config/verifyToken.js
+++ server/src/config/verifyToken.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 
-function authMiddleware (req, res, next) {
+module.exports = function (req, res, next) {
     const token = req.header('auth-token');
     if(!token) return res.status(401).send({error: 'Acesso Negado!'});
 
     try {
         const verified = jwt.verify(token, 'Hu3Lit6NrOpl9Um');
         req.user = verified
+        next();
     } catch (err) {
-        res.status(400).send({error: 'Invalid Token'})
+        res.status(400).send({error: 'Token Inválido!'})
     }
 
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -6,6 +6,7 @@ const CategoriaController = require('./controllers/CategoriaController')
 const AnuncioController = require('./controllers/AnuncioController')
 const UsuarioController = require('./controllers/UsuarioController');
 const AuthController = require('./controllers/AuthController');
+const verifyToken = require('./config/verifyToken');
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -13,11 +14,12 @@ const upload = multer(multerConfig);
 routes.get('/categorias', CategoriaController.index)
 routes.get('/categorias/1', CategoriaController.showCategoria)
 
-routes.get('/usuarios', UsuarioController.index)
+routes.get('/usuarios', verifyToken ,UsuarioController.index)
 routes.get('/profile/:id', UsuarioController.showProfile)
 routes.post('/login', UsuarioController.loginUser)
+routes.post('/register', UsuarioController.createUser);
 
-routes.get('/anuncio', AnuncioController.index)
+routes.get('/anuncio', verifyToken, AnuncioController.index)
 routes.get('/anuncios', AnuncioController.indexPage)
 routes.get('/anuncios/categoria/:id', AnuncioController.indexCategoria)
 
@@ -27,6 +29,5 @@ routes.post('/testeupload', upload.single('file'), (req, res) => {
     return res.json({message: "Imagem cadastrada!"});
 })
 
-routes.post('/register', UsuarioController.createUser);
 
 module.exports = routes;
