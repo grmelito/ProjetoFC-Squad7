c7f9f18ea77eeb69b9312b4b26f322523001b8ea
grmelito <gr.melito@hotmail.com>
2020-06-17 13:25:19 -0300

    Retornando endereço do usuário no perfil

--- server/src/controllers/UsuarioController.js
+++ server/src/controllers/UsuarioController.js
@@ -9,21 +9,29 @@ module.exports = {
     },
 
     async showProfile(req, res) {
-        const token = req.header('auth-token');
-        const decoded = jwt.decode(token, 'Hu3Lit6NrOpl9Um')
-        const id = decoded._id[0].IdUsuario;
+        try {
+            const token = req.header('auth-token');
+            const decoded = jwt.decode(token, 'Hu3Lit6NrOpl9Um')
+            const id = decoded._id[0].IdUsuario;
 
-        const results = await knex('Usuario')
-            .select([
-                'Usuario.Nome',
-                'Usuario.Email',
-                'Usuario.DataNascimento',
-                'Usuario.Genero',
-                'Usuario.ImagemUsuario',
-            ]).where('Usuario.IdUsuario', id);
+            const results = await knex('Usuario')
+                .select([
+                    'Usuario.Nome','Usuario.Email',
+                    'Usuario.DataNascimento','Usuario.Genero',
+                    'Usuario.ImagemUsuario','Endereco.Bairro',
+                    'Endereco.Rua','Endereco.Numero',
+                    'Cidades.CidadeNome','Estados.EstadoNome'
+                ]).join('Endereco', 'Endereco.IdEndereco' ,'=', 'Usuario.IdEndereco')
+                .join('Cidades', 'Cidades.IdCidade', '=', 'Endereco.IdCidade')
+                .join('Estados', 'Estados.IdEstado', '=', 'Cidades.IdEstado')
+                .where('Usuario.IdUsuario', id);
 
-        return results = res.json(results)
+            return results = res.json(results)
+        } catch (err) {
+            return res.status(500)
+        }
     },
+        
 
     async createUser(req, res) {
         //const emailExiste = await knex('Usuario')
@@ -76,6 +84,7 @@ module.exports = {
         })
 
     },
+    
     async updateUser (req, res){
         const {id} = req.params
         const Genero = req.body.Genero
