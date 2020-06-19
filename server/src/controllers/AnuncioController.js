cc7703b77c0b2a4aeb499cd470df2f65363ca9cf
grmelito <gr.melito@hotmail.com>
2020-06-19 16:23:05 -0300

    Criação de anuncio pelo id do token

--- server/src/controllers/AnuncioController.js
+++ server/src/controllers/AnuncioController.js
@@ -52,12 +52,13 @@ module.exports = {
         }
         
     },
+
     async createAnuncio(req, res) {
         const token = req.header('auth-token');
         const decoded = jwt.decode(token, 'Hu3Lit6NrOpl9Um')
-        const id = decoded._id[0].IdUsuario;
+        const id = decoded._id[0].IdFornecedor;
         
-        const data = { Titulo, Descricao, ImagemAnuncio, Telefone, Instagram, Facebook, Site, IdCategoria, IdFornecedor} = req.body
+        const data = { Titulo, Descricao, ImagemAnuncio, Telefone, Instagram, Facebook, Site, IdCategoria } = req.body
         
         const results = await knex('Anuncio')
             .insert({
@@ -68,12 +69,13 @@ module.exports = {
                 Instagram,
                 Facebook,
                 Site,
-                IdCategoria: 1,
-                IdFornecedor: 4
+                IdCategoria,
+                IdFornecedor: id
             });
             return res.json({message: 'Anuncio cadastrado!'})
 
     }, 
+    
     async showAnuncio(req, res){
         const {id} = req.params
 
