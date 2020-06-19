deddfdc61af3d7e28c4a3350767c0737dc54c3ec
EricaSobral <erica.sobral95@gmail.com>
2020-06-19 11:45:56 -0300

    Rotas configuradas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,2 +1,17 @@
-import React from  'react'
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// Impostando Paginas
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/login" component={Login} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
