248fb0e5b755ee40884ef6567cf33b80e64b8670
grmelito <gr.melito@hotmail.com>
2020-06-20 22:38:09 -0300

    Organizando rotas

--- server/src/routes.js
+++ server/src/routes.js
@@ -11,23 +11,22 @@ const verifyToken = require('./config/verifyToken');
 const routes = express.Router();
 const upload = multer(multerConfig);
 
+//routes.get('/categorias/1', CategoriaController.showCategoria)
 routes.get('/categorias', CategoriaController.index)
-routes.get('/categorias/1', CategoriaController.showCategoria)
-
 routes.get('/usuarios', verifyToken, UsuarioController.index)
 routes.get('/profile', verifyToken, UsuarioController.showProfile)
-routes.post('/login', UsuarioController.loginUser)
-routes.post('/register', UsuarioController.createUser);
-routes.put('/update/profile', verifyToken, UsuarioController.updateUser);
-routes.put('/update/profile/image', verifyToken, upload.single('file'), UsuarioController.updateImage)
-
-routes.get('/fornecedores', FornecedorController.index)
-routes.post('/upgrade/usuario', verifyToken, FornecedorController.upgradeFornecedor)
-
 routes.get('/anuncio/:id', AnuncioController.showAnuncio)
 routes.get('/anuncios', AnuncioController.indexPage)
 routes.get('/anuncios/categoria/:id', AnuncioController.indexCategoria)
+routes.get('/fornecedores', FornecedorController.index)
+
+routes.post('/login', UsuarioController.loginUser)
+routes.post('/register', UsuarioController.createUser)
+routes.post('/upgrade/usuario', verifyToken, FornecedorController.upgradeFornecedor)
 routes.post('/anuncio', verifyToken, AnuncioController.createAnuncio)
+
+routes.put('/update/profile', verifyToken, UsuarioController.updateUser)
+routes.put('/update/profile/image', verifyToken, upload.single('file'), UsuarioController.updateImage)
 
 routes.post('/testeupload', upload.single('file'), (req, res) => {
     console.log(req.file);
