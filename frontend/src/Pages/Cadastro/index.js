c3d29d0a575f9b6c97fccf480d8386fa3fe463e7
AnaLauraFeltrim <analaurafeltrim@gmail.com>
2020-06-22 16:26:51 -0300

    cadastro estilizado

--- frontend/src/Pages/Cadastro/index.js
+++ frontend/src/Pages/Cadastro/index.js
@@ -1,56 +1,42 @@
 import React from 'react';
 import '../../assets/css/style.css';
+import QuaseLogo from '../../assets/img/Nome.png'
 
 
 function Cadastro() {
     return (
-        <div>
-            <form class="">
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" />
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" />
-                    </div>
+        <div className="fundo-cadastro">
+            <div className="">
+            <div className="fundo-forms col-9">
+                <div className="logo">
+                    <img src={QuaseLogo} />
+                    <p className="subtitulo">Aproximando pessoas. Fazendo negócios</p>
                 </div>
-                <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity" />
+                <form >
+                    <div className="form-row">
+                        <div className="form-group col-md-12">
+                            <label htmlFor="inputNome">Nome</label>
+                            <input className="form-control" id="inputEmail4" />
+                        </div>
+                        <div className="form-group col-md-12">
+                            <label htmlFor="inputEmail">Email</label>
+                            <input type="email" className="form-control" id="inputEmail4" />
+                        </div>
+                        <div class="form-group col-md-12">
+                            <label htmlFor="inputPassword">Senha</label>
+                            <input type="password" className="form-control" id="inputPassword4" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Cadastre-se</button>
                     </div>
-                    <div class="form-group col-md-4">
-                        <label for="inputState">State</label>
-                        <select id="inputState" class="form-control">
-                            <option selected>Choose...</option>
-                            <option>...</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip" />
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck" />
-                        <label class="form-check-label" for="gridCheck">
-                            Check me out
-      </label>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </form>
+                </form>
+                    <hr></hr>
+                    <p>Já tem uma conta?</p>
+            </div>
+            <p>Ao se cadastrar você concorda com os Termos de Uso e a política de privacidade de BFriend.</p>
+            </div>
+            
+            
+            
         </div>
     )
 } export default Cadastro;
