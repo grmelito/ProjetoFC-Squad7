26073a231c88b8c035e49c958d0cd382dadf2ab9
Carla Dias <carllottadias@gmail.com>
2020-06-24 17:15:20 -0300

    Postando um comentário

--- server/src/controllers/AnuncioController.js
+++ server/src/controllers/AnuncioController.js
@@ -126,5 +126,21 @@ module.exports = {
             IdCategoria: IdCategoria
         })
         return res.json({message: "Informações alteradas!"})
+    }, 
+    async comentarioAnuncio(req, res){
+        const token = req.header('auth-token');
+        const decoded = jwt.decode(token, 'Hu3Lit6NrOpl9Um')
+        const id = decoded._id[0].IdUsuario;
+
+        const idAnuncio = req.params
+        const Comentario = req.body.Comentario
+
+        const dataComentario = await knex('Comentario')
+        .where('Comentario.IdUsuario', id)
+        .insert({
+            Comentario,
+            IdAnuncio: idAnuncio
+        })
+        return res.send({message: "Comentário realizado!"})
     }
 }
