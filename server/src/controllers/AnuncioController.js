e875445463c03e7165ecccbfb521ae5840a5fabb
grmelito <gr.melito@hotmail.com>
2020-06-25 01:12:29 -0300

    Fix de create e show Comentários

--- server/src/controllers/AnuncioController.js
+++ server/src/controllers/AnuncioController.js
@@ -133,12 +133,13 @@ module.exports = {
         })
         return res.json({message: "Informações alteradas!"})
     }, 
+
     async comentarioAnuncio(req, res){
         const token = req.header('auth-token');
         const decoded = jwt.decode(token, 'Hu3Lit6NrOpl9Um')
         const id = decoded._id[0].IdUsuario;
 
-        const idAnuncio = req.params
+        const { idAnuncio } = req.params
         const Comentario = req.body.Comentario
 
         const dataComentario = await knex('Comentario')
@@ -150,6 +151,7 @@ module.exports = {
         })
         return res.send({message: "Comentário realizado!"})
     },
+
     async showComentario(req, res){
     
         const {idAnuncio} = req.params
